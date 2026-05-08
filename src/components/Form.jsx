297244212f8c8bd72cdd45909f1b818cf4bc7053
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-08 16:22:01 +0200

    creati dati per la form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,17 @@
 import { useState } from "react";
+
+    //raccogliere dati inseriti da un utente e inviare a un server tramite post
+
+    /* 1 FASE:  */
+
+    const datiForm = {
+        author : '',  //nome autore
+        title : '', // titolo
+        body : '', 
+        public : false // false perchè per ora è una bozza, sarà true quando metteremo pubblico la checkbox partira' non spuntata
+    };
+
+
 function Form () {
 
 
